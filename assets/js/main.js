c9212ae62d94b32e308fc2492168543c86114390
Thanh Tu <thanhthanhvava2004@gmail.com>
2025-05-14 01:30:51 +0700

    fix: fix background home, collections, specials

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,5 +29,5 @@ const scrollHeader = () => {
   const nav = document.getElementById('header')
   if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener('scroll', scrollHeader);
 
